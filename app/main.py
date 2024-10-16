183ec1aff0ab9d54fd6f900e20edf5db52e965cb
Mikhail Anishchenko <misha321654897i@gmail.com>
2024-10-16 10:49:11 +0300

    update logger config

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 import time
 import logging
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    handlers=[
+                        logging.FileHandler('parser.log', mode='a'),
+                        logging.StreamHandler()
+                    ])
 
 def main():
     while True:
