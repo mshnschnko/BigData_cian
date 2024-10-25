355c150b5cd3eb3114e2e53bf878e839c88e31ae
Timofey-Chibyshev <90447625+Timofey-Chibyshev@users.noreply.github.com>
2024-10-26 01:12:11 +0300

    chto-to changed

--- SP_Parser.py
+++ SP_Parser.py
@@ -1,4 +1,6 @@
 import random
+import re
+
 import cianparser
 import time
 import requests
@@ -9,7 +11,7 @@ from cianparser.constants import METRO_STATIONS
 saintp_parser = cianparser.CianParser(location="Санкт-Петербург")
 
 
-def get_cian_listings_count(url):
+def get_cian_listings_count(url, deal_type):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
     }
@@ -22,21 +24,26 @@ def get_cian_listings_count(url):
         return None
 
     soup = BeautifulSoup(response.content, 'html.parser')
+    with open("parsed_page3.html", "w", encoding="utf-8") as file:
+        file.write(str(soup))
+    print('deal_type:', deal_type)
 
-    count_tag = soup.find('h5', class_='_93444fe79c--color_text-primary-default--vSRPB _93444fe79c--lineHeight_20px--fX7_V _93444fe79c--fontWeight_bold--BbhnX _93444fe79c--fontSize_14px--reQMB _93444fe79c--display_block--KYb25 _93444fe79c--text--e4SBY _93444fe79c--text_letterSpacing__normal--tfToq')
-
-    if count_tag:
-        count_text = count_tag.text
-        count = ''.join(filter(str.isdigit, count_text))
-        return int(count)
+    count_elem = soup.find('h5',
+                           class_='_93444fe79c--color_text-primary-default--vSRPB _93444fe79c--lineHeight_20px--fX7_V _93444fe79c--fontWeight_bold--BbhnX _93444fe79c--fontSize_14px--reQMB _93444fe79c--display_block--KYb25 _93444fe79c--text--e4SBY _93444fe79c--text_letterSpacing__normal--tfToq')
+    # Проверка и извлечение числа из текста
+    if count_elem:
+        count_text = count_elem.get_text(strip=True)  # Извлекаем текст и удаляем лишние пробелы
+        count = int(''.join(filter(str.isdigit, count_text)))  # Оставляем только цифры
+        return count
     else:
         print("Не удалось найти количество объявлений на странице.")
         return None
 
 
-def parse_flats(deal_type="sale", max_pages=45):
+def parse_flats(deal_type="sale", max_pages=45, metro_line='Красная'):
     """
     Парсинг объявлений с сайта Циан по продаже или аренде квартир.
+    :param metro_line:
     :param deal_type: Тип сделки - 'sale' для продажи, 'rent_long' для аренды.
     :param max_pages: Максимальное количество страниц для парсинга.
     :return: Словарь всех объявлений, где ключ - это URL объявления.
@@ -45,13 +52,14 @@ def parse_flats(deal_type="sale", max_pages=45):
     rooms = [1, 2, 3, 4, 5, 'studio']
     floors = [(i, i + 1) for i in range(1, 23, 2)]
     floors.append((23, 1000))
-    stations = [station[0] for station in METRO_STATIONS["Петербургский"]]
-
+    stations = [station[0] for station in METRO_STATIONS["Петербургский"] if station[2] == metro_line]
+    print(stations)
     page_limit = 15  # Максимальное количество страниц в одном запросе
     for start_page in range(1, max_pages + 1, page_limit):
         end_page = min(start_page + page_limit - 1, max_pages)
 
         for station in stations:
+            print('Cur station:', station)
             additional_settings = {
                 "start_page": start_page,
                 "end_page": end_page,
@@ -62,7 +70,7 @@ def parse_flats(deal_type="sale", max_pages=45):
             url = saintp_parser.get_request_url(deal_type=deal_type, rooms=tuple(rooms),
                                                 accommodation_type="flat", additional_settings=additional_settings)
             print("URL:", url)
-            listings_count = get_cian_listings_count(url)
+            listings_count = get_cian_listings_count(url, deal_type)
             if listings_count is None:
                 print(f"Не удалось получить количество объявлений для станции {station}. Пропускаем.")
                 continue
@@ -109,8 +117,8 @@ def parse_flats_for_station(start_page, end_page, station, deal_type, room, addi
 
 
 if __name__ == "__main__":
-    sale_flats_data = parse_flats(deal_type="sale", max_pages=45)
-    rent_flats_data = parse_flats(deal_type="rent_long", max_pages=45)
+    sale_flats_data = parse_flats(deal_type="sale", max_pages=45, metro_line='Красная')
+    rent_flats_data = parse_flats(deal_type="rent_long", max_pages=45, metro_line='Красная')
 
     all_flats_data = {**sale_flats_data, **rent_flats_data}
     print(len(all_flats_data))
